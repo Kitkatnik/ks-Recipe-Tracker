32fbd8c91e9c74658de23b09f31ab1d3dccc15f8
Katya <kittyserenakat@gmail.com>
2022-11-18 15:03:51 -0500

    Add RecipeCreate functionality

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import RecipeData from "./RecipeData"
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
 
-  const createRecipe = () => {};
+  const createRecipe = (newRecipe) => {
+    setRecipes([
+      ...recipes,
+      newRecipe
+    ])
+  };
 
   const deleteRecipe = (indexToDelete) => {
     setRecipes( (currentRecipe) => currentRecipe.filter( (_, index) => index !== indexToDelete));
   };
 
-  // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-  // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+  // DONE: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // DONE: Add the ability for the <RecipeCreate /> component to create new recipes.
 
   
   return (
--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
 
-function RecipeCreate( {createRecipe } ) {
+function RecipeCreate( { createRecipe } ) {
+
+  const initialState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: ""
+  }
+
+  const [newRecipes, setNewRecipe ] = useState(initialState)
+
+  const onChangeHandler = ( { target } ) => {
+    setNewRecipe({
+      ...newRecipes,
+      [target.name]: target.value
+    });
+  };
 
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    createRecipe(newRecipes);
+    setNewRecipe({...initialState});
+  }
   
   return (
-    <form name="create">
+    <form name="create" onSubmit={onSubmitHandler}>
       <table>
         <tbody>
           <tr>
@@ -16,6 +39,9 @@ function RecipeCreate( {createRecipe } ) {
                 name="name"
                 id="name"
                 placeholder="Name"
+                type="text"
+                onChange={onChangeHandler}
+                value={newRecipes.name}
                 required
               />
             </td>
@@ -24,6 +50,9 @@ function RecipeCreate( {createRecipe } ) {
                 name="cuisine" 
                 id="cuisine"
                 placeholder="Cuisine"
+                type="text"
+                onChange={onChangeHandler}
+                value={newRecipes.cuisine}
                 required
               />
             </td>
@@ -32,6 +61,9 @@ function RecipeCreate( {createRecipe } ) {
                 name="photo" 
                 id="photo"
                 placeholder="URL"
+                type="text"
+                onChange={onChangeHandler}
+                value={newRecipes.photo}
                 required
               />
             </td>
@@ -40,6 +72,9 @@ function RecipeCreate( {createRecipe } ) {
                 name="ingredients" 
                 id="ingredients"
                 placeholder="Ingredients"
+                type="text"
+                onChange={onChangeHandler}
+                value={newRecipes.ingredients}
                 required
               />
             </td>
@@ -48,6 +83,9 @@ function RecipeCreate( {createRecipe } ) {
                 name="preparation"
                 id="preparation"
                 placeholder="Preparation"
+                type="text"
+                onChange={onChangeHandler}
+                value={newRecipes.preparation}
                 required
               />
             </td>
